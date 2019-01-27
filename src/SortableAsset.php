385a83cb1d1de5308eb015ef7d4fa072204bdbfc
SPavlov <spavlov@li.ru>
2019-01-27 18:58:20 +0300

    Initial Commit

---
+++ src/SortableAsset.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace coderovich\sortablejs;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for yii-ui/yii2-rubaxa-sortable
+ * Create reorderable drag-and-drop lists for modern browsers and touch devices.
+ *
+ * @see https://github.com/RubaXa/Sortable
+ * @author Christoph Moeke <christophmoeke@gmail.com>
+ * @since 1.0.0
+ */
+class SortableAsset extends AssetBundle {
+	/***
+	 * @inheritdoc
+	 */
+	public $sourcePath = '@bower/sortablejs';
+
+	/**
+	 * Adds JS Files depending on [[YII_ENV_PROD]]
+	 */
+	public function init() {
+		parent::init();
+
+		$this->js = [
+			(YII_ENV_DEV ? 'Sortable.min.js' : 'Sortable.js')
+		];
+	}
+
+	/***
+	 * @inheritdoc
+	 */
+	public $depends = [
+		'yii\web\JqueryAsset',
+	];
+}
